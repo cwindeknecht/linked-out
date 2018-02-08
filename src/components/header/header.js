4080d0a78b3aec92ded304419427f344158462a9
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-02-07 19:17:17 -0500

    Header 'finished'
    Header.js/Header.css --- updated to look okay, no functionality what so ever.
    
    Also added all the images for the header.

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import './header.css'
+import './header.css';
+import logo from '../../img/logo.png';
+import home from '../../img/home.png';
+import job from '../../img/job.png';
+import me from '../../img/me.png';
+import message from '../../img/message.png';
+import my from '../../img/my.png';
+import notification from '../../img/notification.png';
 
 function Header(props) {
     return (
         <div className='header'>
-            <img src='../../../img/logo.png' />
+            <img className='logo' src={logo}/>
             <form>
-                <input style={{textAlign:'center'}}
+                <input style={{textAlign:'center', margin:'5px'}}
                     placeholder='Search'
                     type='text'
                 />
             </form>
+            <img className='link' src={home}/>
+            <img className='link' src={my}/>
+            <img className='link' src={job}/>
+            <img className='link' src={message}/>
+            <img className='link' src={notification}/>
+            <img className='link' src={me}/>
         </div>
     );
 }
